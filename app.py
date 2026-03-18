2d5cb2634022d4fb43294b9554026ebd991275d0
emily-seaman <emily.seaman1@education.nsw.gov.au>
2026-03-18 23:50:03 +0000

    Implemented app.py from offline editor

--- app.py
+++ app.py
@@ -3,5 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def index():
+    return render_template("index.html")
+
+@app.route("/focus")
+def focus():
+    return render_template("focus.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
